91c9d8700400333df89362938c88022647cea41d
stevleibelt <artodeto@arcor.de>
2013-08-26 16:52:39 +0200

    Created log entry collection

---
+++ source/Net/Bazzline/Component/LogLevelTriggeredLogger/LogEntryCollection.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author stev leibelt <artodeot@arcor.de>
+ * @since 2013-08-26
+ */
+
+namespace Net\Bazzline\Component\LogLevelTriggered\Logger;
+
+use SplObjectStorage;
+
+/**
+ * Class LogEntryCollection
+ *
+ * @package Net\Bazzline\Component\LogLevelTriggered\Logger
+ * @author stev leibelt <artodeot@arcor.de>
+ * @since 2013-08-26
+ */
+class LogEntryCollection extends SplObjectStorage
+{
+    /**
+     * @param LogEntryInterface $entry
+     * @return $this
+     * @author stev leibelt <artodeot@arcor.de>
+     * @since 2013-08-26
+     */
+    public function add(LogEntryInterface $entry)
+    {
+        $this->attach($entry);
+
+        return $this;
+    }
+}
