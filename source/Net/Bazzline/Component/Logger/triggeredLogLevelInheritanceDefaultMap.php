93fc756c06d9c58fadfcc17c0caa463f7af82240
stevleibelt <stev@leibelt.de>
2013-08-26 22:49:50 +0200

    Created default map

---
+++ source/Net/Bazzline/Component/Logger/triggeredLogLevelInheritanceDefaultMap.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-26 
+ */
+
+use Psr\Log\LogLevel;
+
+return array(
+    LogLevel::DEBUG => array(
+        LogLevel::INFO,
+        LogLevel::NOTICE,
+        LogLevel::WARNING,
+        LogLevel::ERROR,
+        LogLevel::CRITICAL,
+        LogLevel::ALERT,
+        LogLevel::EMERGENCY
+    ),
+    LogLevel::INFO => array(
+        LogLevel::NOTICE,
+        LogLevel::WARNING,
+        LogLevel::ERROR,
+        LogLevel::CRITICAL,
+        LogLevel::ALERT,
+        LogLevel::EMERGENCY
+    ),
+    LogLevel::NOTICE => array(
+        LogLevel::WARNING,
+        LogLevel::ERROR,
+        LogLevel::CRITICAL,
+        LogLevel::ALERT,
+        LogLevel::EMERGENCY
+    ),
+    LogLevel::WARNING => array(
+        LogLevel::ERROR,
+        LogLevel::CRITICAL,
+        LogLevel::ALERT,
+        LogLevel::EMERGENCY
+    ),
+    LogLevel::ERROR => array(
+        LogLevel::CRITICAL,
+        LogLevel::ALERT,
+        LogLevel::EMERGENCY
+    ),
+    LogLevel::ALERT => array(
+        LogLevel::EMERGENCY
+    )
+);
