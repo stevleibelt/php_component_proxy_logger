f2aa3bd09f645590539ad670c29b28e9dd5613cf
stevleibelt <artodeto@arcor.de>
2013-08-27 10:17:42 +0200

    Excluded trigger handling to separat interface

---
+++ source/Net/Bazzline/Component/Logger/LogLevelTriggeredLoggerInterface.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-26
+ */
+
+namespace Net\Bazzline\Component\Logger;
+
+/**
+ * Class LogLevelTriggeredLoggerInterface
+ *
+ * @package Net\Bazzline\Component\Logger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-26
+ */
+interface LogLevelTriggeredLoggerInterface
+{
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToEmergency();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToAlert();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToCritical();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToError();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToWarning();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToNotice();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToInfo();
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevelToDebug();
+
+    /**
+     * @param mixed $level
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggerLevel($level);
+
+    /**
+     * @param array $map
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setTriggeredLogLevelInheritanceMap(array $map);
+}
