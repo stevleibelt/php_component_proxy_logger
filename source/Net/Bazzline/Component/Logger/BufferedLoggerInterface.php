da41a6531e43352b5c5d239431ba9a0542de6ce8
stevleibelt <stev@leibelt.de>
2013-08-27 20:48:33 +0200

    Finished BufferedLogger

--- source/Net/Bazzline/Component/Logger/BufferedLoggerInterface.php
+++ source/Net/Bazzline/Component/Logger/BufferedLoggerInterface.php
@@ -13,6 +13,6 @@ namespace Net\Bazzline\Component\Logger;
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-08-27
  */
-interface BufferLoggerInterface extends ProxyLoggerInterface, LogEntryBufferAwareInterface, LogEntryFactoryInjectInterface
+interface BufferedLoggerInterface extends ProxyLoggerInterface, LogEntryBufferAwareInterface, LogEntryFactoryInjectInterface
 {
 }
