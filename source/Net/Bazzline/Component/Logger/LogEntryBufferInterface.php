8428f528e33403c621d981e214b188315ebe3207
stevleibelt <stev@leibelt.de>
2013-08-27 19:40:22 +0200

    Created interface

---
+++ source/Net/Bazzline/Component/Logger/LogEntryBufferInterface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 8/27/13
+ */
+
+namespace Net\Bazzline\Component\Logger;
+
+/**
+ * Class LogEntryBufferInterface
+ *
+ * @package Net\Bazzline\Component\Logger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-27
+ */
+interface LogEntryBufferInterface
+{
+    /**
+     * @param LogEntryInterface $entry
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function attach(LogEntryInterface $entry);
+
+    /**
+     * @param LogEntryInterface $entry
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function contains(LogEntryInterface $entry);
+
+    /**
+     * @param LogEntryInterface $entry
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function detach(LogEntryInterface $entry);
+}
