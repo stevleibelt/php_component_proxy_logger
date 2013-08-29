06a9f4d4e4d5134677096ac2bff4add366c2d866
stevleibelt <stev@leibelt.de>
2013-08-29 23:59:12 +0200

    Scribbled possible solution for handling threshold for two directions

---
+++ source/Net/Bazzline/Component/Logger/LogLevelThreshold.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-29 
+ */
+
+namespace Net\Bazzline\Component\Logger;
+
+/**
+ * Class LogLevelThreshold
+ *
+ * @package Net\Bazzline\Component\Logger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-29
+ */
+class LogLevelThreshold implements LogLevelTriggerThresholdInterface
+{
+    /**
+     * @var array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     */
+    protected $map;
+
+    /**
+     * @var
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     */
+    protected $transformedMap;
+
+    /**
+     * @param array $logLevelToThresholdMap
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     * @todo add validation for map?
+     */
+    public function __construct(array $logLevelToThresholdMap)
+    {
+        $this->map = $logLevelToThresholdMap;
+        $this->transformedMap = array();
+
+        foreach ($this->map as $logLevelTrigger => $logLevelThresholds) {
+            $this->transformedMap[$logLevelTrigger] = array_flip($logLevelThresholds);
+        }
+    }
+
+    /**
+     * @param mixed $logLevel
+     * @param mixed $logLevelTrigger
+     * @return bool
+     * @throws InvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     */
+    public function isThresholdReached($logLevel, $logLevelTrigger)
+    {
+        return (isset($this->transformedMap[$logLevelTrigger])
+                && isset($this->transformedMap[$logLevelTrigger][$logLevel]));
+    }
+}
---
+++ source/Net/Bazzline/Component/Logger/LogLevelTriggerThresholdInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-29 
+ */
+
+namespace Net\Bazzline\Component\Logger;
+
+/**
+ * Class LogLevelTriggerThresholdInterface
+ *
+ * @package Net\Bazzline\Component\Logger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-29
+ */
+interface LogLevelTriggerThresholdInterface
+{
+    /**
+     * @param array $logLevelToThresholdMap
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     */
+    public function __construct(array $logLevelToThresholdMap);
+
+    /**
+     * @param mixed $logLevel
+     * @param mixed $logLevelTrigger
+     * @return bool
+     * @throws InvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-29
+     */
+    public function isThresholdReached($logLevel, $logLevelTrigger);
+}
