97f16af326b9f6cf395ebe6c5d75cf09bbc16bf9
stevleibelt <stev@leibelt.de>
2013-09-06 12:04:47 +0200

    Added unittests for AlwaysAvoidBuffer and NeverAvoidBuffer

---
+++ tests/Test/Net/Bazzline/Component/Logger/BufferManipulation/AlwaysAvoidBufferTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author sleibelt
+ * @since 2013-09-06
+ */
+
+namespace Test\Net\Bazzline\Component\Logger\BufferManipulation;
+
+use Net\Bazzline\Component\Logger\BufferManipulation\AlwaysAvoidBuffer;
+use Psr\Log\LogLevel;
+use Test\Net\Bazzline\Component\DataType\TestCase;
+
+/**
+ * Class AlwaysAvoidBufferTest
+ *
+ * @package Test\Net\Bazzline\Component\Logger\BufferManipulation
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-09-05
+ */
+class AlwaysAvoidBufferTest extends TestCase
+{
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public static function testCaseDataProvider()
+    {
+        return array(
+            'no log level set no avoided' => array(
+                'logLevel' => null,
+                'logLevelToAvoid' => null,
+                'expectedAvoidBuffering' => true
+            ),
+            'log level set but not avoided' => array(
+                'logLevelToAdd' => LogLevel::INFO,
+                'logLevelToAvoid' => null,
+                'expectedAvoidBuffering' => true
+            ),
+            'log level set but different avoided' => array(
+                'logLevelToAdd' => LogLevel::DEBUG,
+                'logLevelToAvoid' => LogLevel::INFO,
+                'expectedAvoidBuffering' => true
+            ),
+            'log level set and same to avoid' => array(
+                'logLevelToAdd' => LogLevel::INFO,
+                'logLevelToAvoid' => LogLevel::INFO,
+                'expectedAvoidBuffering' => true
+            )
+        );
+    }
+
+    /**
+     * @dataProvider testCaseDataProvider
+     *
+     * @param mixed $logLevel
+     * @param mixed $logLevelToAvoid
+     * @param bool $expectedAvoidBuffering
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function testAvoidBuffering($logLevel, $logLevelToAvoid, $expectedAvoidBuffering)
+    {
+        $avoidBuffer = new AlwaysAvoidBuffer();
+        if (!is_null($logLevelToAvoid)) {
+            $avoidBuffer->addAvoidableLogLevel($logLevelToAvoid);
+        }
+
+        $this->assertEquals($expectedAvoidBuffering, $avoidBuffer->avoidBuffering($logLevel));
+    }
+}
---
+++ tests/Test/Net/Bazzline/Component/Logger/BufferManipulation/NeverAvoidBufferTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author sleibelt
+ * @since 2013-09-06
+ */
+
+namespace Test\Net\Bazzline\Component\Logger\BufferManipulation;
+
+use Net\Bazzline\Component\Logger\BufferManipulation\NeverAvoidBuffer;
+use Psr\Log\LogLevel;
+use Test\Net\Bazzline\Component\DataType\TestCase;
+
+/**
+ * Class NeverAvoidBufferTest
+ *
+ * @package Test\Net\Bazzline\Component\Logger\BufferManipulation
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-09-05
+ */
+class NeverAvoidBufferTest extends TestCase
+{
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public static function testCaseDataProvider()
+    {
+        return array(
+            'no log level set no avoided' => array(
+                'logLevel' => null,
+                'logLevelToAvoid' => null,
+                'expectedAvoidBuffering' => false
+            ),
+            'log level set but not avoided' => array(
+                'logLevelToAdd' => LogLevel::INFO,
+                'logLevelToAvoid' => null,
+                'expectedAvoidBuffering' => false
+            ),
+            'log level set but different avoided' => array(
+                'logLevelToAdd' => LogLevel::DEBUG,
+                'logLevelToAvoid' => LogLevel::INFO,
+                'expectedAvoidBuffering' => false
+            ),
+            'log level set and same to avoid' => array(
+                'logLevelToAdd' => LogLevel::INFO,
+                'logLevelToAvoid' => LogLevel::INFO,
+                'expectedAvoidBuffering' => false
+            )
+        );
+    }
+
+    /**
+     * @dataProvider testCaseDataProvider
+     *
+     * @param mixed $logLevel
+     * @param mixed $logLevelToAvoid
+     * @param bool $expectedAvoidBuffering
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function testAvoidBuffering($logLevel, $logLevelToAvoid, $expectedAvoidBuffering)
+    {
+        $avoidBuffer = new NeverAvoidBuffer();
+        if (!is_null($logLevelToAvoid)) {
+            $avoidBuffer->addAvoidableLogLevel($logLevelToAvoid);
+        }
+
+        $this->assertEquals($expectedAvoidBuffering, $avoidBuffer->avoidBuffering($logLevel));
+    }
+}
