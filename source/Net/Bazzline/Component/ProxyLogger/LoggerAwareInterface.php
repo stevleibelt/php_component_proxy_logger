d89ac6c1115d0a83483bfe9dcaa57615caa0d573
stevleibelt <stev@leibelt.de>
2013-09-08 18:48:44 +0200

    Added LoggerAwareInterface

---
+++ source/Net/Bazzline/Component/ProxyLogger/LoggerAwareInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-09-08 
+ */
+
+namespace Net\Bazzline\Component\ProxyLogger;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerAwareInterface as ParentInterface;
+
+/**
+ * Class LoggerAwareInterface
+ *
+ * @package Net\Bazzline\Component\ProxyLogger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-09-08
+ */
+interface LoggerAwareInterface extends ParentInterface
+{
+    /**
+     * @return null|LoggerInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-08
+     */
+    public function getLogger();
+
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-08
+     */
+    public function hasLogger();
+}
