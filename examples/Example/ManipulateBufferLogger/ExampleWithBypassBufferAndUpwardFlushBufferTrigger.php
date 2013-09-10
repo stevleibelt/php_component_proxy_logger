b71a39bbab6e32657a7d1944238af56b94539ee9
stevleibelt <stev@leibelt.de>
2013-09-10 20:34:52 +0200

    updated to new factory interface

--- examples/Example/ManipulateBufferLogger/ExampleWithBypassBufferAndUpwardFlushBufferTrigger.php
+++ examples/Example/ManipulateBufferLogger/ExampleWithBypassBufferAndUpwardFlushBufferTrigger.php
@@ -48,13 +48,10 @@ class ExampleWithBypassBufferAndUpwardFlushBufferTrigger
     {
         $bypassBuffer = new BypassBuffer();
         $logger = new OutputToConsoleLogger();
-        $logRequestFactory = new LogRequestFactory();
-        $logRequestFactory->setLogRequestClassName('LogRequest');
-        $logRequestBufferFactory = new LogRequestRuntimeBufferFactory();
         $manipulateBufferLoggerFactory = new ManipulateBufferLoggerFactory();
         $flushBufferTrigger = new UpwardFlushBufferTrigger();
 
-        $this->logger = $manipulateBufferLoggerFactory->create($logger, $logRequestFactory, $logRequestBufferFactory, $flushBufferTrigger, $bypassBuffer);
+        $this->logger = $manipulateBufferLoggerFactory->create($logger, null, null, $flushBufferTrigger, $bypassBuffer);
 
         return $this;
     }
