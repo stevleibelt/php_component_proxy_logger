be0ab956a836c607b2e5771ac5b35e6674898f3c
stevleibelt <stev@leibelt.de>
2013-10-13 20:22:29 +0200

    Starting updating buffer logger factory

--- source/Net/Bazzline/Component/ProxyLogger/Factory/BufferLoggerFactory.php
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/BufferLoggerFactory.php
@@ -19,6 +19,20 @@ use Psr\Log\LoggerInterface;
 class BufferLoggerFactory implements BufferLoggerFactoryInterface
 {
     /**
+     * @var LogRequestFactoryInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-10
+     */
+    protected $logRequestFactory;
+
+    /**
+     * @var LogRequestBufferFactoryInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-10
+     */
+    protected $logRequestBufferFactory;
+
+    /**
      * If not provided, following factories are used as default.
      *  - LogRequestFactory with log request class name of LogRequest
      *  - LogRequestRuntimeBufferFactory
@@ -48,5 +62,71 @@ class BufferLoggerFactory implements BufferLoggerFactoryInterface
         $bufferLogger->setLogRequestBufferFactory($logRequestBufferFactory);
 
         return $bufferLogger;
+    }
+
+    /**
+     * @return null|LogRequestBufferFactoryInterface $factory
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function getLogRequestBufferFactory()
+    {
+        return $this->logRequestBufferFactory;
+    }
+
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function hasLogRequestBufferFactory()
+    {
+        return (!is_null($this->logRequestBufferFactory));
+    }
+
+    /**
+     * @param LogRequestBufferFactoryInterface $factory
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-27
+     */
+    public function setLogRequestBufferFactory(LogRequestBufferFactoryInterface $factory)
+    {
+        $this->logRequestBufferFactory = $factory;
+
+        return $this;
+    }
+
+    /**
+     * @return null|LogRequestFactoryInterface $factory
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function getLogRequestFactory()
+    {
+        return $this->logRequestFactory;
+    }
+
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-05
+     */
+    public function hasLogRequestFactory()
+    {
+        return (!is_null($this->logRequestFactory));
+    }
+
+    /**
+     * @param LogRequestFactoryInterface $factory
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-26
+     */
+    public function setLogRequestFactory(LogRequestFactoryInterface $factory)
+    {
+        $this->logRequestFactory = $factory;
+
+        return $this;
     }
 }
--- source/Net/Bazzline/Component/ProxyLogger/Factory/BufferLoggerFactoryInterface.php
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/BufferLoggerFactoryInterface.php
@@ -15,7 +15,7 @@ use Psr\Log\LoggerInterface;
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-09-08
  */
-interface BufferLoggerFactoryInterface
+interface BufferLoggerFactoryInterface extends LogRequestFactoryAwareInterface, LogRequestBufferFactoryAwareInterface
 {
     /**
      * @param LoggerInterface $logger
