1d8f8afa4e6f410b17df61f7decfbf60157495b9
stevleibelt <stev@leibelt.de>
2013-10-14 22:10:54 +0200

    Started implementing log levels to bypass

--- source/Net/Bazzline/Component/ProxyLogger/Factory/BypassBufferFactory.php
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/BypassBufferFactory.php
@@ -7,6 +7,7 @@
 namespace Net\Bazzline\Component\ProxyLogger\Factory;
 
 use Net\Bazzline\Component\ProxyLogger\BufferManipulator\BypassBuffer;
+use Net\Bazzline\Component\ProxyLogger\Validator\IsValidLogLevel;
 
 /**
  * Class BypassBufferFactory
@@ -18,6 +19,13 @@ use Net\Bazzline\Component\ProxyLogger\BufferManipulator\BypassBuffer;
 class BypassBufferFactory implements BypassBufferFactoryInterface
 {
     /**
+     * @var array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-14
+     */
+    protected $logLevelsToBypass;
+
+    /**
      * @return \Net\Bazzline\Component\ProxyLogger\BufferManipulator\BypassBufferInterface
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-10-13
@@ -26,6 +34,32 @@ class BypassBufferFactory implements BypassBufferFactoryInterface
     {
         $bypassBuffer = new BypassBuffer();
 
+        foreach ($this->logLevelsToBypass as $logLevelToBypass) {
+            $bypassBuffer->addBypassForLogLevel($logLevelToBypass);
+        }
+
         return $bypassBuffer;
+    }
+
+    /**
+     * @param array $logLevelsToBypass
+     * @return $this
+     * @throws \Net\Bazzline\Component\ProxyLogger\Exception\RuntimeException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-14
+     */
+    public function setLogLevelsToBypass(array $logLevelsToBypass)
+    {
+        $validator = new IsValidLogLevel();
+
+        foreach ($logLevelsToBypass as $logLevelToBypass) {
+            $validator->setLogLevel($logLevelsToBypass);
+
+            $validator->isMet();
+        }
+
+        $this->logLevelsToBypass = $logLevelsToBypass;
+
+        return $this;
     }
 }
--- source/Net/Bazzline/Component/ProxyLogger/Factory/BypassBufferFactoryInterface.php
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/BypassBufferFactoryInterface.php
@@ -21,4 +21,13 @@ interface BypassBufferFactoryInterface
      * @since 2013-10-13
      */
     public function create();
+
+    /**
+     * @param array $logLevelsToBypass
+     * @return $this
+     * @throws \Net\Bazzline\Component\ProxyLogger\Exception\RuntimeException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-14
+     */
+    public function setLogLevelsToBypass(array $logLevelsToBypass);
 }
