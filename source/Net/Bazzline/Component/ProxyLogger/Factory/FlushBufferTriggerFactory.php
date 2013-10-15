3805ff467328e22d0c438a1191bcb1c8421c3843
stevleibelt <stev@leibelt.de>
2013-10-15 20:19:08 +0200

    Scribbled empty FlushBufferTriggerFactory

---
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/FlushBufferTriggerFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-15
+ */
+
+namespace Net\Bazzline\Component\ProxyLogger\Factory;
+
+/**
+ * Class FlushBufferTriggerFactory
+ *
+ * @package Net\Bazzline\Component\ProxyLogger\Factory
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-15
+ */
+class FlushBufferTriggerFactory implements FlushBufferTriggerFactoryInterface
+{
+    /**
+     * @return \Net\Bazzline\Component\ProxyLogger\BufferManipulator\FlushBufferTriggerInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-13
+     */
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * @param string $logLevel
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-15
+     */
+    public function setTriggerToLogLevel($logLevel)
+    {
+        // TODO: Implement setTriggerToLogLevel() method.
+    }
+}
