a026618076ea32439187a88fd4cc34b281c54a91
stevleibelt <stev@leibelt.de>
2013-10-15 20:18:58 +0200

    Added method to set trigger

--- source/Net/Bazzline/Component/ProxyLogger/Factory/FlushBufferTriggerFactoryInterface.php
+++ source/Net/Bazzline/Component/ProxyLogger/Factory/FlushBufferTriggerFactoryInterface.php
@@ -21,4 +21,12 @@ interface FlushBufferTriggerFactoryInterface
      * @since 2013-10-13
      */
     public function create();
+
+    /**
+     * @param string $logLevel
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-15
+     */
+    public function setTriggerToLogLevel($logLevel);
 }
