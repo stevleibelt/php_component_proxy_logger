d95fcaa9e40ad2f91a773d205e46af094cf8cef8
stevleibelt <stev@leibelt.de>
2013-10-27 22:30:10 +0100

    Added example

---
+++ examples/Example/BufferLogger/ExampleWithDefaultBufferLoggerFactory.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-28
+ */
+
+namespace Example\BufferLogger;
+
+use Net\Bazzline\Component\ProxyLogger\Factory\DefaultBufferLoggerFactory;
+use Net\Bazzline\Component\ProxyLogger\OutputToConsoleLogger;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+ExampleWithDefaultBufferLoggerFactory::create()
+    ->setup()
+    ->andRun();
+
+/**
+ * Class Example
+ *
+ * @package Example\BufferLogger
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-28
+ */
+class ExampleWithDefaultBufferLoggerFactory
+{
+    /**
+     * @var \Net\Bazzline\Component\ProxyLogger\Proxy\BufferLogger
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-28
+     */
+    private $bufferLogger;
+
+    /**
+     * @return Example
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-28
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    /**
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-28
+     */
+    public function setup()
+    {
+        $bufferLoggerFactory = new DefaultBufferLoggerFactory();
+        $logger = new OutputToConsoleLogger();
+
+        $this->bufferLogger = $bufferLoggerFactory->create($logger);
+
+        return $this;
+    }
+
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-28
+     */
+    public function andRun()
+    {
+        echo str_repeat('-', 40) . PHP_EOL;
+        echo 'Adding logging messages' . PHP_EOL;
+        $this->bufferLogger->info('Current line is ' . __LINE__);
+        $this->bufferLogger->alert('Current line is ' . __LINE__);
+        $this->bufferLogger->critical('Current line is ' . __LINE__);
+        echo str_repeat('-', 40) . PHP_EOL;
+        echo 'Flush buffer' . PHP_EOL;
+        $this->bufferLogger->flush();
+        echo str_repeat('-', 40) . PHP_EOL;
+    }
+}
