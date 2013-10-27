872ca922f64aa62b11bfde0f9a88bda25c44144b
stevleibelt <stev@leibelt.de>
2013-10-27 22:42:54 +0100

    added defualt buffer logger factory with test

--- tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/DefaultBufferLoggerFactoryTest.php
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/DefaultBufferLoggerFactoryTest.php
@@ -6,17 +6,36 @@
 
 namespace Test\Net\Bazzline\Component\ProxyLogger\Factory;
 
+use Net\Bazzline\Component\ProxyLogger\OutputToConsoleLogger;
 use Net\Bazzline\Component\ProxyLogger\Factory\DefaultBufferLoggerFactory;
 use Test\Net\Bazzline\Component\ProxyLogger\TestCase;
 
+/**
+ * Class DefaultBufferLoggerFactoryTest
+ *
+ * @package Test\Net\Bazzline\Component\ProxyLogger\Factory
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-26
+ */
 class DefaultBufferLoggerFactoryTest extends TestCase
 {
-    public function testNew()
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-26
+     */
+    public function testCreate()
     {
         $factory = new DefaultBufferLoggerFactory();
-        $logger = $factory->create($this->getNewPsr3LoggerMock());
+        $logger = $this->getNewPsr3LoggerMock();
+        $logger->shouldReceive('log')
+            ->with('info', 'test message', array())
+            ->once();
 
-        //test that logger mock gets expected method calls
-        //$this->assertTrue($logger->)
+        $bufferLogger = $factory->create($logger);
+        $bufferLogger->info('test message');
+        $bufferLogger->flush();
+
+        $this->assertInstanceOf('Net\Bazzline\Component\ProxyLogger\Proxy\BufferLoggerInterface', $bufferLogger);
+        $this->assertInstanceOf('Net\Bazzline\Component\ProxyLogger\Proxy\BufferLogger', $bufferLogger);
     }
 } 
