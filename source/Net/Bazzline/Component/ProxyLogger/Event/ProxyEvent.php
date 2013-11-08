d0e300bb37485bb6ac4685228b51f21cbaa5b656
stevleibelt <stev@leibelt.de>
2013-11-08 23:40:45 +0100

    added event

--- source/Net/Bazzline/Component/ProxyLogger/Event/ProxyEvent.php
+++ source/Net/Bazzline/Component/ProxyLogger/Event/ProxyEvent.php
@@ -6,6 +6,7 @@
 
 namespace Net\Bazzline\Component\ProxyLogger\Event;
 
+use Psr\Log\LoggerInterface;
 use Net\Bazzline\Component\ProxyLogger\LogRequest\LogRequestInterface;
 
 /**
@@ -18,6 +19,8 @@ use Net\Bazzline\Component\ProxyLogger\LogRequest\LogRequestInterface;
 class ProxyEvent extends Event
 {
     const LOG_LOG_REQUEST = 'proxyEvent.logLogRequest';
+    const LOG_LOG_REQUEST_POST = 'proxyEvent.logLogRequestPost';
+
     /**
      * @var LogRequestInterface
      * @author stev leibelt <artodeto@arcor.de>
@@ -26,14 +29,14 @@ class ProxyEvent extends Event
     private $logRequest;
 
     /**
-     * @var array
+     * @var LoggerInterface[]
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-11-08
      */
     private $loggerCollection;
 
     /**
-     * @return array
+     * @return LoggerInterface[]
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-11-08
      */
@@ -52,7 +55,13 @@ class ProxyEvent extends Event
         return $this->logRequest;
     }
 
-    public function setLoggerCollection(array $loggerCollection)
+    /**
+     * @param LoggerInterface[] $loggerCollection
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function setLoggerCollection($loggerCollection)
     {
         $this->loggerCollection = $loggerCollection;
 
