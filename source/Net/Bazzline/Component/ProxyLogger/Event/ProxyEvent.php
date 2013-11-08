71ef3ad8759e61bcf7fef1212f08c77f472a0aed
stevleibelt <stev@leibelt.de>
2013-11-08 23:21:35 +0100

    Added methods

--- source/Net/Bazzline/Component/ProxyLogger/Event/ProxyEvent.php
+++ source/Net/Bazzline/Component/ProxyLogger/Event/ProxyEvent.php
@@ -6,6 +6,8 @@
 
 namespace Net\Bazzline\Component\ProxyLogger\Event;
 
+use Net\Bazzline\Component\ProxyLogger\LogRequest\LogRequestInterface;
+
 /**
  * Class ProxyEvent
  *
@@ -16,4 +18,57 @@ namespace Net\Bazzline\Component\ProxyLogger\Event;
 class ProxyEvent extends Event
 {
     const LOG_LOG_REQUEST = 'proxyEvent.logLogRequest';
-} 
+    /**
+     * @var LogRequestInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    protected $logRequest;
+
+    /**
+     * @var array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    protected $loggerCollection;
+
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function getLoggerCollection()
+    {
+        return $this->loggerCollection;
+    }
+
+    /**
+     * @return LogRequestInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function getLogRequest()
+    {
+        return $this->logRequest;
+    }
+
+    public function setLoggerCollection(array $loggerCollection)
+    {
+        $this->loggerCollection = $loggerCollection;
+
+        return $this;
+    }
+
+    /**
+     * @param LogRequestInterface $logRequest
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function setLogRequest(LogRequestInterface $logRequest)
+    {
+        $this->logRequest = $logRequest;
+
+        return $this;
+    }
+}
