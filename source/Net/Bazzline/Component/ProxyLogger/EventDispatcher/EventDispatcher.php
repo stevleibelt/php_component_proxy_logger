c43ad395f01415cd08ac8ed7676d930f1d58bbd9
stevleibelt <stev@leibelt.de>
2013-11-08 23:25:33 +0100

    wrapped EventDispatcher

---
+++ source/Net/Bazzline/Component/ProxyLogger/EventDispatcher/EventDispatcher.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-08
+ */
+
+namespace Net\Bazzline\Component\ProxyLogger\EventDispatcher;
+
+use Symfony\Component\EventDispatcher\EventDispatcher as ParentClass;
+
+/**
+ * Class EventDispatcher
+ *
+ * @package Net\Bazzline\Component\ProxyLogger\EventDispatcher
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-08
+ */
+class EventDispatcher extends ParentClass
+{
+} 
