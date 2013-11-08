b7745e5f74f996fd075e59521b13679f623af763
stevleibelt <stev@leibelt.de>
2013-11-08 23:40:56 +0100

    finished draft of event listener

---
+++ source/Net/Bazzline/Component/ProxyLogger/EventListener/ProxyListener.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-08
+ */
+
+namespace Net\Bazzline\Component\ProxyLogger\EventListener;
+
+use Net\Bazzline\Component\ProxyLogger\Event\ProxyEvent;
+use Net\Bazzline\Component\ProxyLogger\EventDispatcher\EventDispatcher;
+
+/**
+ * Class ProxyListener
+ *
+ * @package Net\Bazzline\Component\ProxyLogger\EventListener
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-08
+ */
+class ProxyListener implements EventListenerInterface
+{
+    /**
+     * @param EventDispatcher $eventDispatcher
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function attach(EventDispatcher $eventDispatcher)
+    {
+        $eventDispatcher->addListener(ProxyEvent::LOG_LOG_REQUEST, array($this, 'logRequest'));
+
+        return $this;
+    }
+
+    /**
+     * @param EventDispatcher $eventDispatcher
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function detach(EventDispatcher $eventDispatcher)
+    {
+        $eventDispatcher->removeListener(ProxyEvent::LOG_LOG_REQUEST, array($this, 'logRequest'));
+
+        return $this;
+    }
+
+    /**
+     * @param ProxyEvent $event
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function logRequest(ProxyEvent $event)
+    {
+        $loggerCollection = $event->getLoggerCollection();
+        $logRequest = $event->getLogRequest();
+
+        foreach ($loggerCollection as $logger) {
+            $logger->log(
+                $logRequest->getLevel(),
+                $logRequest->getMessage(),
+                $logRequest->getContext()
+            );
+        }
+
+        $event->getDispatcher()->dispatch(ProxyEvent::LOG_LOG_REQUEST_POST, $event);
+    }
+}
