37d62a50ba5a51ba9e55a756de78af423d1647df
stevleibelt <stev@leibelt.de>
2013-11-09 00:57:16 +0100

    finished another scribble

--- source/Net/Bazzline/Component/ProxyLogger/EventListener/BufferListener.php
+++ source/Net/Bazzline/Component/ProxyLogger/EventListener/BufferListener.php
@@ -10,6 +10,13 @@ use Net\Bazzline\Component\ProxyLogger\Event\BufferEvent;
 use Net\Bazzline\Component\ProxyLogger\Event\ProxyEvent;
 use Net\Bazzline\Component\ProxyLogger\EventDispatcher\EventDispatcher;
 
+/**
+ * Class BufferListener
+ *
+ * @package Net\Bazzline\Component\ProxyLogger\EventListener
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-09
+ */
 class BufferListener implements EventListenerInterface
 {
     /**
@@ -36,8 +43,8 @@ class BufferListener implements EventListenerInterface
     public function detach(EventDispatcher $eventDispatcher)
     {
         $eventDispatcher->removeListener(BufferEvent::ADD_LOG_REQUEST_TO_BUFFER, array($this, 'addLogRequestToBuffer'));
-        $eventDispatcher->addListener(BufferEvent::BUFFER_CLEAN, array($this, 'bufferClean'));
-        $eventDispatcher->addListener(BufferEvent::BUFFER_FLUSH, array($this, 'bufferFlush'));
+        $eventDispatcher->removeListener(BufferEvent::BUFFER_CLEAN, array($this, 'bufferClean'));
+        $eventDispatcher->removeListener(BufferEvent::BUFFER_FLUSH, array($this, 'bufferFlush'));
 
         return $this;
     }
@@ -82,12 +89,7 @@ class BufferListener implements EventListenerInterface
             $dispatcher = $event->getDispatcher();
 
             foreach ($buffer as $logRequest) {
-                $proxyEvent = new ProxyEvent(
-                    $logRequest,
-                    $event->getLoggerCollection()
-                );
-
-                $dispatcher->dispatch(ProxyEvent::LOG_LOG_REQUEST, $proxyEvent);
+                $dispatcher->dispatch(ProxyEvent::LOG_LOG_REQUEST, $event);
             }
 
             $dispatcher->dispatch(BufferEvent::BUFFER_CLEAN, $event);
---
+++ source/Net/Bazzline/Component/ProxyLogger/EventListener/ManipulateBufferListener.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-09
+ */
+
+namespace Net\Bazzline\Component\ProxyLogger\EventListener;
+
+use Net\Bazzline\Component\ProxyLogger\Event\BufferEvent;
+use Net\Bazzline\Component\ProxyLogger\Event\ManipulateBufferEvent;
+use Net\Bazzline\Component\ProxyLogger\Event\ProxyEvent;
+use Net\Bazzline\Component\ProxyLogger\EventDispatcher\EventDispatcher;
+
+/**
+ * Class ManipulateBufferListener
+ *
+ * @package Net\Bazzline\Component\ProxyLogger\EventListener
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-09
+ */
+class ManipulateBufferListener implements EventListenerInterface
+{
+    /**
+     * @param EventDispatcher $eventDispatcher
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function attach(EventDispatcher $eventDispatcher)
+    {
+        $eventDispatcher->addListener(ProxyEvent::LOG_LOG_REQUEST_PRE, array($this, 'logLogRequestPre'));
+    }
+
+    /**
+     * @param EventDispatcher $eventDispatcher
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-08
+     */
+    public function detach(EventDispatcher $eventDispatcher)
+    {
+        $eventDispatcher->removeListener(ManipulateBufferEvent::LOG_LOG_REQUEST_PRE, array($this, 'logLogRequestPre'));
+    }
+
+    /**
+     * @param ManipulateBufferEvent $event
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-09
+     */
+    public function logLogRequestPre(ManipulateBufferEvent $event)
+    {
+        if (!$event->isPropagationStopped()) {
+            $logRequest = $event->getLogRequest();
+            $dispatcher = $event->getDispatcher();
+            $wasNotHandledByFlushBufferTrigger = true;
+
+            if ($event->hasFlushBufferTrigger()) {
+                $flushBufferTrigger = $event->getFlushBufferTrigger();
+                if ($flushBufferTrigger->triggerBufferFlush($logRequest->getLevel())) {
+                    $dispatcher->dispatch(ManipulateBufferEvent::ADD_LOG_REQUEST_TO_BUFFER, $event);
+                    $dispatcher->dispatch(ManipulateBufferEvent::BUFFER_FLUSH, $event);
+                    $wasNotHandledByFlushBufferTrigger = false;
+                }
+            }
+
+            if ($wasNotHandledByFlushBufferTrigger
+                && $event->hasBypassBuffer()) {
+                $bypassBuffer = $event->getBypassBuffer();
+                if ($bypassBuffer->bypassBuffer($logRequest->getLevel())) {
+                    $dispatcher->dispatch(ManipulateBufferEvent::LOG_LOG_REQUEST, $event);
+                }
+            }
+        }
+    }
+}
