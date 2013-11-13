2c8cfe3b1689a9bb750aa3b66b0e1864c5bf2b48
stevleibelt <stev@leibelt.de>
2013-11-14 00:03:58 +0100

    fixed ProxyLoggerTest

---
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Logger/AbstractLoggerTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-11-13 
+ */
+
+namespace Test\Net\Bazzline\Component\ProxyLogger\Logger;
+
+
+class AbstractLoggerTest {
+
+}
--- tests/Test/Net/Bazzline/Component/ProxyLogger/Logger/ProxyLoggerTest.php
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Logger/ProxyLoggerTest.php
@@ -6,6 +6,7 @@
 
 namespace Test\Net\Bazzline\Component\ProxyLogger\Logger;
 
+use Net\Bazzline\Component\ProxyLogger\Event\ProxyEvent;
 use Net\Bazzline\Component\ProxyLogger\Logger\ProxyLogger;
 use Psr\Log\LogLevel;
 use Test\Net\Bazzline\Component\ProxyLogger\TestCase;
@@ -39,168 +40,26 @@ class ProxyLoggerTest extends TestCase
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-08-28
      */
-    public function testAddLogger()
-    {
-        $logger = $this->getNewLogger();
-
-        $this->assertEquals($logger, $logger->addLogger($this->getNewPsr3LoggerMock()));
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-10-23
-     */
-    public function testGetLoggers()
-    {
-        $proxyLogger = $this->getNewLogger();
-        $logger = $this->getNewPsr3LoggerMock();
-        $this->assertNull($proxyLogger->getLoggers());
-
-        $proxyLogger->addLogger($logger);
-        $this->assertEquals(array($logger), $proxyLogger->getLoggers());
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testEmergency()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::EMERGENCY, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->emergency($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testAlert()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::ALERT, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->alert($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testCritical()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::CRITICAL, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->critical($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testError()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::ERROR, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->error($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testWarning()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::WARNING, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->warning($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testNotice()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::NOTICE, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->notice($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testInfo()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::INFO, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->info($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
-    public function testDebug()
-    {
-        $logger = $this->getNewLogger();
-        $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::DEBUG, $this->message, array())
-            ->once();
-
-        $logger->addLogger($realLogger);
-        $logger->debug($this->message);
-    }
-
-    /**
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-08-28
-     */
     public function testLog()
     {
         $logger = $this->getNewLogger();
+        $request = $this->getNewLogRequestMock();
         $realLogger = $this->getNewPsr3LoggerMock();
-        $realLogger->shouldReceive('log')
-            ->with(LogLevel::INFO, $this->message, array())
+        $factory = $this->getNewLogRequestFactoryMock($request);
+        $event = $this->getNewEventMock();
+        $event->shouldReceive('setLogRequest')
+            ->once();
+        $event->shouldReceive('setLoggerCollection')
+            ->once();
+        $dispatcher = $this->getNewEventDispatcherMock();
+        $dispatcher->shouldReceive('dispatch')
+            ->with(ProxyEvent::LOG_LOG_REQUEST, $event)
             ->once();
 
         $logger->addLogger($realLogger);
+        $logger->setEvent($event);
+        $logger->setEventDispatcher($dispatcher);
+        $logger->setLogRequestFactory($factory);
         $logger->log(LogLevel::INFO, $this->message);
     }
 
