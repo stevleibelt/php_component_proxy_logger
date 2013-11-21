468d3393bd08fd7f810eccef6edf64b3907767ef
stevleibelt <stev@leibelt.de>
2013-11-21 23:02:04 +0100

    started test for proxyEventFactory

--- tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ProxyEventFactoryTest.php
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ProxyEventFactoryTest.php
@@ -6,6 +6,7 @@
 
 namespace Test\Net\Bazzline\Component\ProxyLogger\Factory;
 
+use Net\Bazzline\Component\ProxyLogger\Factory\ProxyEventFactory;
 use Test\Net\Bazzline\Component\ProxyLogger\TestCase;
 
 /**
@@ -19,9 +20,17 @@ class ProxyEventFactoryTest extends TestCase
     /**
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-11-18
+     * @todo create testcases
+     *  - (loggerCollection^!logRequest)
+     *  - (!loggerCollection^logRequest)
+     *  - (loggerCollection^logRequest)
      */
     public function testCreate()
     {
-        $this->markTestIncomplete('todo');
+        $factory = new ProxyEventFactory();
+        $event = $factory->create();
+
+        $this->assertSame(array(), $event->getLoggerCollection());
+        $this->assertNull($event->getLogRequest());
     }
 } 
