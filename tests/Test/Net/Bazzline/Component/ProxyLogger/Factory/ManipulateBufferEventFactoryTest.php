5d4660fd429311b6eed35108469861649ec891e6
stevleibelt <stev@leibelt.de>
2013-11-21 23:14:53 +0100

    Started manipulate buffer event factory test

--- tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ManipulateBufferEventFactoryTest.php
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ManipulateBufferEventFactoryTest.php
@@ -6,6 +6,7 @@
 
 namespace Test\Net\Bazzline\Component\ProxyLogger\Factory;
 
+use Net\Bazzline\Component\ProxyLogger\Factory\ManipulateBufferEventFactory;
 use Test\Net\Bazzline\Component\ProxyLogger\TestCase;
 
 /**
@@ -19,9 +20,23 @@ class ManipulateBufferEventFactoryTest extends TestCase
     /**
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-11-19
+     * @todo implement test cases
+     *  - (hasBypassBuffer^!hasFlushBufferTrigger^!hasLoggerCollection^!hasLogRequestBuffer^!hasLogRequest)
+     *  - (!hasBypassBuffer^hasFlushBufferTrigger^!hasLoggerCollection^!hasLogRequestBuffer^!hasLogRequest)
+     *  - ...
+     *  - (hasBypassBuffer^hasFlushBufferTrigger^hasLoggerCollection^hasLogRequestBuffer^hasLogRequest)
      */
     public function testCreate()
     {
-        $this->markTestIncomplete('todo');
+        $factory = new ManipulateBufferEventFactory();
+        $event = $factory->create();
+
+        $this->assertFalse($event->hasBypassBuffer());
+        $this->assertFalse($event->hasFlushBufferTrigger());
+        $this->assertSame(array(), $event->getLoggerCollection());
+        $this->assertNull($event->getBypassBuffer());
+        $this->assertNull($event->getFlushBufferTrigger());
+        $this->assertNull($event->getLogRequestBuffer());
+        $this->assertNull($event->getLogRequest());
     }
 }
