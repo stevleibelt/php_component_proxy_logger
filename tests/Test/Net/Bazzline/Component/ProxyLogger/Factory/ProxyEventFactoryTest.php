9f37f6886f610811c6766e037e6fab85dd31e651
stevleibelt <stev@leibelt.de>
2013-11-22 17:50:07 +0100

    prepared for extended unittest

--- tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ProxyEventFactoryTest.php
+++ tests/Test/Net/Bazzline/Component/ProxyLogger/Factory/ProxyEventFactoryTest.php
@@ -18,6 +18,57 @@ use Test\Net\Bazzline\Component\ProxyLogger\TestCase;
 class ProxyEventFactoryTest extends TestCase
 {
     /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-11-22
+     */
+    public static function createTestCaseDataProvider()
+    {
+        $preconditions = array(
+            'setLoggerCollection' => true,
+            'setLogRequest' => true
+        );
+        $expectations = array(
+            'hasLoggerCollection' => true,
+            'hasLogRequest' => true
+        );
+        $testCases = array(
+            'has logger and request' => array(
+                'preconditions' => array(),
+                'expectations' => array()
+            ),
+            'has logger and no request' => array(
+                'preconditions' => array(
+                    'setLogRequest' => false
+                ),
+                'expectations' => array(
+                    'hasLogRequest' => false
+                )
+            ),
+            'has no logger and request' => array(
+                'preconditions' => array(
+                    'setLoggerCollection' => false
+                ),
+                'expectations' => array(
+                    'hasLoggerCollection' => false
+                )
+            ),
+            'has no logger and no request' => array(
+                'preconditions' => array(
+                    'setLoggerCollection' => false,
+                    'setLogRequest' => false
+                ),
+                'expectations' => array(
+                    'hasLoggerCollection' => false,
+                    'hasLogRequest' => false
+                )
+            )
+        );
+
+        return self::mergeTestCasesWithDefaults($testCases, $preconditions, $expectations);
+    }
+
+    /**
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-11-18
      * @todo create testcases
